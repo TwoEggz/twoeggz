bf2607920e97b7ec8d56ad7277493f7d7b155fb5
TwoEggz <twoeggz@gmail.com>
2019-01-03 20:56:58 +0800

    2

---
+++ cx.max.js
@@ -0,0 +1,113 @@
+
+
+
+//https://www.bidvertiser.com/
+/*if(!isMobile.spider)
+{
+if(!isMobile.any)
+{
+	$(".dan").css("width","728px");
+	document.write('<script data-cfasync="false" src="https://bdv.bidvertiser.com/BidVertiser.dbm?pid=823236&bid=1965595" type="text/javascript" async="async"></script>');
+}
+else
+{
+	$(".dan").css("width","468px");
+	document.write('<script data-cfasync="false" src="https://bdv.bidvertiser.com/BidVertiser.dbm?pid=823236&bid=1965596" type="text/javascript" async="async"></script>');
+}
+}*/
+
+//https://yllix.com/
+/*if(width>=728)
+{
+	$(".dan").css("width","728px");
+	document.write('<script type="text/javascript" src="https://ylx-1.com/bnr.php?section=General&pub=726684&format=728x90&ga=g"></script>');
+}
+else if(width<728)
+{
+	$(".dan").css("width","300px");
+	document.write('<script type="text/javascript" src="https://ylx-1.com/bnr.php?section=General&pub=726684&format=300x50&ga=g&mbtodb=1"></script>');
+}*/
+
+
+
+/*if(!isMobile.spider)
+{
+	if(!isMobile.any)
+{
+	//document.write('<script src="https://jc.anhuilitian.net/7c792a/c@64665!3.js"></script>');
+	document.write('<script src="https://jc.anhuilitian.net/7c792a/v@64793!17.js"><\/script>');
+}
+else
+{
+	//document.write('<script src="https://jc.anhuilitian.net/7c792a/c@64663!22.js"></script>');
+	document.write('<script src="https://jc.anhuilitian.net/7c792a/x@64795!33.js"><\/script>');
+}
+}
+
+*/
+
+//https://www.chinesean.com/
+/*
+if(!isMobile.spider)
+{
+if(!isMobile.any)
+{
+	$(".dan").css("width","720px");
+	$(".dan").html("<iframe src='https://www.chinesean.com/affiliate/box.do?bid=18232' width='720' height='90' frameBorder='0' border='0' scrolling='no'></iframe>");
+}
+else
+{
+	$(".dan").css("width","300px");
+	$(".dan").html("<iframe src='https://www.chinesean.com/affiliate/box.do?bid=18256' width='300' height='250' frameBorder='0' border='0' scrolling='no'></iframe>");
+}
+}*/
+
+/*
+if(!isMobile.spider)
+{
+	if(!isMobile.any)
+{
+	document.write("<script type='text/javascript'>var ufid=51015;var ufdomain='//js.jianbaimei.com';document.write('<scr'+'ipt type=\"text/javascript\" src=\"'+ufdomain+'/Include/data\"></scri'+'pt>');</script>");
+}
+else
+{
+	document.write("<script type='text/javascript'>var ufid=51016;var ufdomain='//js.jianbaimei.com';document.write('<scr'+'ipt type=\"text/javascript\" src=\"'+ufdomain+'/Include/data\"></scri'+'pt>');</script>");
+}
+}*/
+
+
+if(!isMobile.spider)
+{
+var pcad=new Array();
+pcad[0]="http://wm.lrswl.com/page/?s=291693";//¿œÈ≈ ˜
+var mobad=new Array();
+mobad[0]="https://m.dpjszs.com/d.php?pid=1700";// cnweb.qztiange.com
+
+	if(!isMobile.any)
+{
+	document.write("<script src='"+pcad[Math.floor(Math.random()*(pcad.length-1+1))]+"'></script>");
+}
+else
+{
+	document.write("<script src='"+mobad[Math.floor(Math.random()*(mobad.length-1+1))]+"'></script>");
+}
+}
+
+/*if(!isMobile.spider)
+{
+	var pcad=new Array("https://ws1.sinaimg.cn/large/6fbea7e8gy1fx7lhqd3svj20oq046ac1.jpg","https://ws1.sinaimg.cn/large/6fbea7e8gy1fx7li8awwmj20oq046jt4.jpg","https://ws1.sinaimg.cn/large/6fbea7e8gy1fx7liopl6uj20oq04675u.jpg","https://ws1.sinaimg.cn/large/6fbea7e8gy1fx7lj5rcd9j20oq046abv.jpg","https://ws1.sinaimg.cn/large/6fbea7e8gy1fx7ljk92lgj20oq046tai.jpg","https://ws1.sinaimg.cn/large/6fbea7e8gy1fx7lk5llzxj20oq046gnh.jpg","https://ws1.sinaimg.cn/large/6fbea7e8gy1fx7lkk8mx8j20oq046406.jpg","https://ws1.sinaimg.cn/large/6fbea7e8gy1fx7lkxorqxj20oq046mz1.jpg","https://ws1.sinaimg.cn/large/6fbea7e8gy1fx7llcnthgj20oq046abp.jpg","https://ws1.sinaimg.cn/large/6fbea7e8gy1fx7llt26tjj20oq046dhl.jpg","https://ws1.sinaimg.cn/large/6fbea7e8gy1fx7lm5858xj20oq046abl.jpg","https://ws1.sinaimg.cn/large/6fbea7e8gy1fx7lmhmca0j20oq046gn3.jpg");
+	var mobad=new Array("https://ws1.sinaimg.cn/large/6fbea7e8gy1fx7kw325n5g20hs046wg4.gif","https://ws1.sinaimg.cn/large/6fbea7e8gy1fx7kwf1evmg20hs0460tz.gif","https://ws1.sinaimg.cn/large/6fbea7e8gy1fx7kwlntvzg20hs046mzo.gif","https://ws1.sinaimg.cn/large/6fbea7e8gy1fx7kwptkc9g20hs046778.gif","https://ws1.sinaimg.cn/large/6fbea7e8gy1fx7kwt2cb8g20hs046wg1.gif");
+	if(!isMobile.any)
+{
+	$(".dan").css("width","890px");
+	$(".dan").html("<a href='http://www.llyq8866.com/?palcode=1001110939' target='_blank' rel='nofollow'><img src='"+pcad[Math.floor(Math.random()*(pcad.length-1+1))]+"'/></a>");
+}
+else
+{
+	$(".dan").css({"position":"fixed","bottom":"0px","width":"100%","z-index":"9999"});
+	$(".dan img").css("width","100%");
+	$(".dan").html("<a href='http://m.llyq8866.com/?palcode=1001110939' target='_blank' rel='nofollow'><img src='"+mobad[Math.floor(Math.random()*(mobad.length-1+1))]+"'/></a>");
+}
+}
+
+*/
